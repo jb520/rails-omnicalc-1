8418a4252a87f736659bd316ec9bebadceb2a0e3
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-04 20:57:55 +0000

    create /square/new path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "calculator", :action => "process_square"})
+
+  get("/square/new", {:controller => "calculator", :action => "process_square"})
   
 end
