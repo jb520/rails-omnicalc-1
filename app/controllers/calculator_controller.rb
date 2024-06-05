1de84117633a3a19b7d39caa0e371b9c758358de
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-05 17:00:56 +0000

    add results pages

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,9 +1,61 @@
 class CalculatorController < ApplicationController
-  def "square"
+  def square
     render({:template => "cal_pages/cal_square"})
   end
 
-  def "square_root"
+  def square_root
     render({:template => "cal_pages/cal_sqrt"})
+  end
+
+  def payment
+    render({:template => "cal_pages/cal_payment"})
+  end
+
+  def new_random
+    render({:template => "cal_pages/cal_random"})
+  end
+
+  # results
+  
+  def result_sq
+    @num = params.fetch("number").to_f
+    @result = @num ** 2
+    
+    render({:template => "cal_pages/sq_result"})
+  end
+
+  def result_sqrt
+    @num = params.fetch("number").to_f
+    @result = Math.sqrt(@num)
+
+    render({:template => "cal_pages/sqrt_result"})
+  end
+
+  def result_payment
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_i
+    @pv = params.fetch("user_pv").to_f
+
+    #apr to rate per month
+    @rate = @apr / 100
+    @r = @rate / 12 
+
+    #years to number of periods
+    @n = @years * 12
+
+    @numerator = @r * @pv
+    @denominator = 1 - ((1 + @r) ** (-@n))
+    
+    @result = @numerator / @denominator
+
+    render({:template => "cal_pages/payment_result"})
+  end
+
+  def result_random
+    @min = params.fetch("user_min").to_f
+    @max = params.fetch("user_max").to_f
+    @result = rand(@min..@max)
+
+    render({:template => "cal_pages/random_result"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
+  # form pages
+
   get("/", {:controller => "calculator", :action => "square"})
 
   get("/square/new", {:controller => "calculator", :action => "square"})
   
   get("/square_root/new", {:controller => "calculator", :action => "square_root"})
+
+  get("/payment/new", {:controller => "calculator", :action => "payment"})
+
+  get("/random/new", {:controller => "calculator", :action => "new_random"})
+
+  # results pages
+  
+  get("/square/results", {:controller => "calculator", :action => "result_sq"})
+
+  get("/square_root/results", {:controller => "calculator", :action => "result_sqrt"})
+
+  get("/payment/results", {:controller => "calculator", :action => "result_payment"})
+
+  get("/random/results", {:controller => "calculator", :action => "result_random"})
 end
